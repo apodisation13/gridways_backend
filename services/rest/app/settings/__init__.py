f8c0e05c1542c613e4595ab5c7dda2ebf227c079
apodisation13 <apodisation13@gmail.com>
2025-12-02 17:04:58 +0300

    rest

--- services/rest/app/settings/__init__.py
+++ services/rest/app/settings/__init__.py
@@ -1,15 +1,15 @@
-from .security import *
-from .apps import *
-from .middleware import *
-from .templates import *
-from .database import *
-from .localization import *
-from .static import *
-# from .urls import *
+from .apps import *  # noqa 403
+from .database import *  # noqa 403
+from .localization import *  # noqa 403
+from .middleware import *  # noqa 403
+from .security import *  # noqa 403
+from .static import *  # noqa 403
+from .templates import *  # noqa 403
+
 
 # Базовые настройки
-ROOT_URLCONF = 'settings.urls'
-WSGI_APPLICATION = 'settings.wsgi.application'
-ASGI_APPLICATION = 'settings.asgi.application'
+ROOT_URLCONF = "settings.urls"
+WSGI_APPLICATION = "settings.wsgi.application"
+ASGI_APPLICATION = "settings.asgi.application"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
