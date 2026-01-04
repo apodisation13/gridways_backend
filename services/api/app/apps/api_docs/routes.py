1fd414b820be9f3bf6c107aa10f7dd3d866687e8
apodisation13 <apodisation13@gmail.com>
2026-01-04 11:33:30 +0300

    only developer can access swagger

--- services/api/app/apps/api_docs/routes.py
+++ services/api/app/apps/api_docs/routes.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
-from services.api.app.apps.auth.schemas import UserRegister
 from services.api.app.apps.auth.service import AuthService
 from services.api.app.dependencies import get_auth_service
 from starlette.responses import HTMLResponse
@@ -15,10 +14,9 @@ security_basic = HTTPBasic()
 
 async def check_developer_credentials(
     credentials: HTTPBasicCredentials = Depends(security_basic),
-    service: AuthService = Depends(get_auth_service),
-) -> UserRegister:
-    user = await service._get_user_from_db(email=credentials.username)
-    return user
+    auth_service: AuthService = Depends(get_auth_service),
+) -> None:
+    await auth_service.get_developer_user(email=credentials.username)
 
 
 @router.get("/docs", include_in_schema=False)
