fa732752537356fbfbe0ab03733f9ded029ad177
apodisation13 <apodisation13@gmail.com>
2026-01-10 18:02:32 +0300

    user_database

--- services/api/app/dependencies.py
+++ services/api/app/dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI
 from lib.utils.db.pool import Database
 from services.api.app.apps.auth.service import AuthService
 from services.api.app.apps.news.service import NewsService
+from services.api.app.apps.progress.service import UserProgressService
 from services.api.app.config import Config
 
 
@@ -39,6 +40,16 @@ async def get_news_service(
     config: Config = Depends(get_config),
 ) -> NewsService:
     return NewsService(
+        db_pool=db_pool,
+        config=config,
+    )
+
+
+async def get_user_progress_service(
+    db_pool: Database = Depends(get_db),
+    config: Config = Depends(get_config),
+) -> UserProgressService:
+    return UserProgressService(
         db_pool=db_pool,
         config=config,
     )
