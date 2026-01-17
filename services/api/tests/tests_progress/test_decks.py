811cec75dec9ab73cfd5845a71227c7f917bea5a
apodisation13 <apodisation13@gmail.com>
2026-01-17 08:37:02 +0300

    tests step 1

--- services/api/tests/tests_progress/test_decks.py
+++ services/api/tests/tests_progress/test_decks.py
@@ -3,53 +3,53 @@ import pytest
 from httpx import AsyncClient
 
 
-class TestDecksAPI:
-    @pytest.mark.asyncio
-    async def test_create_user_deck(
-        self,
-        client: AsyncClient,
-        db_connection,
-        user_factory,
-    ) -> None:
-        user = await user_factory(
-            # email="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-        )
-        print("STR35", type(user), user.id)
-
-        response = await client.post(
-            "/users/register-user",
-            json={
-                "email": "email",
-                "password": "password",
-                "username": "username",
-            }
-        )
-
-        a = await db_connection.fetch("SELECT * FROM users")
-        print("STR40", a)
-
-        response_json = response.json()
-        print("STR38", response_json)
-        assert response.status_code == 200
-
-        response = await client.post(
-            "/users/login-user",
-            json={
-                "email": "email",
-                "password": "password",
-            }
-        )
-
-        response_json = response.json()
-        print("STR38", response_json)
-        assert response.status_code == 200
-
-        token = response_json["token"]["access_token"]
-        print(58, token)
-
-        response = await client.get(
-            "/user-progress/1",
-            headers={"Authorization": f"Bearer {token}"},
-        )
-        response_json = response.json()
-        print("STR38", response_json)
+# class TestDecksAPI:
+#     @pytest.mark.asyncio
+#     async def test_create_user_deck(
+#         self,
+#         client: AsyncClient,
+#         db_connection,
+#         user_factory,
+#     ) -> None:
+#         user = await user_factory(
+#             # email="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+#         )
+#         print("STR35", type(user), user.id)
+#
+#         response = await client.post(
+#             "/users/register-user",
+#             json={
+#                 "email": "email",
+#                 "password": "password",
+#                 "username": "username",
+#             }
+#         )
+#
+#         a = await db_connection.fetch("SELECT * FROM users")
+#         print("STR40", a)
+#
+#         response_json = response.json()
+#         print("STR38", response_json)
+#         assert response.status_code == 200
+#
+#         response = await client.post(
+#             "/users/login-user",
+#             json={
+#                 "email": "email",
+#                 "password": "password",
+#             }
+#         )
+#
+#         response_json = response.json()
+#         print("STR38", response_json)
+#         assert response.status_code == 200
+#
+#         token = response_json["token"]["access_token"]
+#         print(58, token)
+#
+#         response = await client.get(
+#             "/user-progress/1",
+#             headers={"Authorization": f"Bearer {token}"},
+#         )
+#         response_json = response.json()
+#         print("STR38", response_json)
